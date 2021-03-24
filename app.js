37f852455f65ce418f025afb4718cb30825354a3
uo269450 <UO269450@uniovi.es>
2021-03-24 10:26:12 +0100

    “SDI-2021-103-7.4-Recursos estáticos, peticiones POST y parámetros, Enrutamiento y comodines.

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 let express = require('express');
 let app = express();
 
+let bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use(express.static('public'));
+
 // Variables
 app.set('port', 8081)
 
