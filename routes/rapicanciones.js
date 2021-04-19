a531e639b60f15aa5495faabeedd7f34efcf6d4e
uo269450 <UO269450@uniovi.es>
2021-04-19 18:15:02 +0200

    SDI-2021-103-10.2-SW API REST Autenticación

--- routes/rapicanciones.js
+++ routes/rapicanciones.js
@@ -82,6 +82,34 @@ module.exports = function(app, gestorBD) {
         });
     });
 
+    app.post("/api/autenticar", function (req,res){
+        let seguro= app.get("crypto"). createHmac('sha256', app.get('clave')).update(req.body.password).digest('hex');
+        let criterio={
+            email: req.body.email,
+            password: seguro
+        }
+
+        gestorBD.obtenerUsuarios(criterio, function (usuarios){
+            if(usuarios==null|| usuarios.length==0){
+                res.status(401);
+                res.json({
+                    autenticado:false
+                });
+            }
+            else{
+
+                let token = app.get('jwt').sign(
+                    {usuario: criterio.email , tiempo: Date.now()/1000},
+                    "secreto");
+                res.status(200);
+                res.json({
+                    autenticado:true,
+                    token: token
+                })
+            }
+        })
+    });
+
     app.delete("/api/cancion/:id", function(req, res) {
         let criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id)}
 
